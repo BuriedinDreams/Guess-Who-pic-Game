b0601b516f45b562bdf993f340540c3db6c9c8e0
Zack Werner <allthelight044@gmail.com>
2021-02-01 20:54:47 -0600

    pretty stuck, might start from scratch for appending people to the dom

--- client.js
+++ client.js
@@ -8,14 +8,26 @@ function onReady() {
   profilePics();
 }
 
-function profilePics() {
+function profilePics(people) {
   $('.imgOfPeople').append(`
+  <div class="pics">
   <img src="./profilePictures/DustyM.png" alt="Image of Dusty" sizes="250">
-  <img src="./profilePictures/JohnS.png" alt="Image of John" sizes="250">
-  <img src="./profilePictures/JoshG.png" alt="Image of Josh" sizes="250">
+  </div class="pics">
+  <div>
+  <img src="./profilePictures/JohnS.png" alt="Image of John" sizes="250" id="John">
+  </div class="pics">
+  <div>
+  <img src="./profilePictures/JoshG.png" alt="Image of Josh" sizes="250" >
+  </div class="pics">
+  <div class="pics">
   <img src="./profilePictures/McKynlee.png" alt="Image of McKynlee" sizes="250">
+  </div>
   <img src="./profilePictures/PatrickN.png" alt="Image of Patrick" sizes="250">
+  <div class="pics">
   <img src="./profilePictures/Vada.png" alt="Image of Vada" sizes="250">
+  </div>
+  <div class="pics">
   <img src="./profilePictures/WilliamKrug.png" alt="Image of William" sizes="250">
+  </div>
   `);
 }
