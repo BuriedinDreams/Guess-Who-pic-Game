f742e90d32f09ce89fe67814d9b5ad9f51f429a3
Zack Werner <allthelight044@gmail.com>
2021-02-01 20:12:48 -0600

    have all the imgs ready to append to dom

--- client.js
+++ client.js
@@ -4,4 +4,18 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log('Inside jQuery');
+
+  $(document).on('load', profilePics);
+}
+
+function profilePics() {
+  $('.imgOfPeople').append(`
+  <img src="./profilePictures/DustyM.png" alt="Image of Dusty" sizes="250">
+  <img src="./profilePictures/JohnS.png" alt="Image of John" sizes="250">
+  <img src="./profilePictures/JoshG.png" alt="Image of Josh" sizes="250">
+  <img src="./profilePictures/McKynlee.png" alt="Image of McKynlee" sizes="250">
+  <img src="./profilePictures/PatrickN.png" alt="Image of Patrick" sizes="250">
+  <img src="./profilePictures/Vada.png" alt="Image of Vada" sizes="250">
+  <img src="./profilePictures/WilliamKrug.png" alt="Image of William" sizes="250">
+  `);
 }
